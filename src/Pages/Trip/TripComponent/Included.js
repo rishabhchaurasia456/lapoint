ed4792405d04a956aac8ca3859547f8c36b44877
Rishabh Chaurasia <rishabhchaurasia456@gmail.com>
2024-09-09 10:55:11 +0530

    innner_page

--- src/Pages/Trip/TripComponent/Included.js
+++ src/Pages/Trip/TripComponent/Included.js
@@ -1,81 +1,425 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import questionIcon from "../../../Images/question-mark-outline-in-a-circle-hand-drawn-button.png";
+import carIcon from "../../../Images/car.png";
+import wineIcon from "../../../Images/glass-with-wine.png";
+import homeIcon from "../../../Images/home.png";
+import checkIcon from "../../../Images/check-mark.png";
+import waterIcon from "../../../Images/water-waves.png";
 
 const Included = () => {
+  // Set initial state to "1 week" for all levels
+  const [selectedOption, setSelectedOption] = useState({
+    level1: "1 week",
+    level2: "1 week",
+    level3: "1 week",
+  });
 
-  const openTabSection = (evt, tabName) => {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabs_item");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+  // Data for each level's options, including images
+  const levelData = {
+    level1: {
+      "1 week": {
+        price: "v.a. € 949 / week",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "9 uur kiteles met 2 cursisten / 1 set / 1 instructeur",
+          },
+          {
+            image: checkIcon,
+            text: "shared rental voor 1 week met het nieuwste kitemateriaal",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel incl. ontbijt",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+      "10 Days": {
+        price: "v.a. € 1049 / 10 days",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "9 uur kiteles met 2 cursisten / 1 set / 1 instructeur",
+          },
+          {
+            image: checkIcon,
+            text: "shared rental voor 1 week met het nieuwste kitemateriaal",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel incl. ontbijt",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+      "2 Weeks": {
+        price: "v.a. € 1199 / 2 weeks",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "9 uur kiteles met 2 cursisten / 1 set / 1 instructeur",
+          },
+          {
+            image: checkIcon,
+            text: "shared rental voor 1 week met het nieuwste kitemateriaal",
+          },
+          {
+            image: checkIcon,
+            text: "voor de 2e week rental complete set incl. waterstart supervisie upgrade.",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+    },
+    level2: {
+      "1 week": {
+        price: "v.a. € 1099 / week",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "Opfris cursus, theorie, veiligheid etc.",
+          },
+          {
+            image: checkIcon,
+            text: "Supervisie en coaching zodat je zelfstandig kunt oefenen",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+      "10 Days": {
+        price: "v.a. € 1249 / 10 days",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "Opfriscursus, theorie en veiligheid",
+          },
+          {
+            image: checkIcon,
+            text: "Supervisie, tips en clinics zodat. je veilig zelfstandig kunt oefenen.",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel incl. ontbijt",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+      "2 Weeks": {
+        price: "v.a. € 1499 / 2 weeks",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "Opfriscursus, theorie, veiligheid",
+          },
+          {
+            image: checkIcon,
+            text: "Supervisie & coaching",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel incl. ontbijt",
+          },
+          {
+            image: wineIcon,
+            text: "heerlijk lokaal ontbijt en heerlijk avondeten",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+    },
+    level3: {
+      "1 week": {
+        price: "v.a. € 1999 / week",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: checkIcon,
+            text: "Coaching en trick clinics. Maak progressie!",
+          },
+          {
+            image: homeIcon,
+            text: "overnachting in kitehotel",
+          },
+          {
+            image: wineIcon,
+            text: "delicious local breakfast and delicious dinner",
+          },
+          {
+            image: waterIcon,
+            text: "kitecenter faciliteiten & spot use",
+          },
+          {
+            image: carIcon,
+            text: "airport transfer",
+          },
+          {
+            image: carIcon,
+            text: "gezelligheid, foto’s & fun!",
+          },
+        ]
+      },
+      "10 Days": {
+        price: "v.a. € 2249 / 10 days",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: questionIcon,
+            text: "9 uur kiteles met 2 cursisten / 1 set / 1 instructeur",
+          },
+          {
+            image: questionIcon,
+            text: "shared rental voor 1 week met het nieuwste kitemateriaal",
+          },
+        ]
+      },
+      "2 Weeks": {
+        price: "v.a. € 2499 / 2 weeks",
+        para: [
+          {
+            image: questionIcon,
+            text: "Is dit pakket wel geschikt voor mij?",
+          },
+          {
+            image: questionIcon,
+            text: "9 uur kiteles met 2 cursisten / 1 set / 1 instructeur",
+          },
+          {
+            image: questionIcon,
+            text: "shared rental voor 1 week met het nieuwste kitemateriaal",
+          },
+        ]
+      },
+    },
+  };
 
-    tablinks = document.getElementsByTagName("li");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("current", "");
-    }
-
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += "current";
+  // Handle button click for each level
+  const handleButtonClick = (level, option) => {
+    setSelectedOption((prev) => ({ ...prev, [level]: option }));
   };
 
   return (
     <div>
-      <div className="container">
+      {/* <div className="container">
         <div className="row">
-          <p className='included_desc py-4'>We organize the perfect kitesurfing holiday according to the Hakuna Matata principle, what 'No stress' means in Afrikaans 🙂 See below what is included in this trip and what you can add to optimally tailor your trip to what you want to achieve!</p>
+          <p className='included_desc py-4'>
+            We organize the perfect kitesurfing holiday according to the Hakuna Matata principle,
+            what 'No stress' means in Afrikaans 🙂 See below what is included in this trip and what
+            you can add to optimally tailor your trip to what you want to achieve!
+          </p>
         </div>
-      </div>
+      </div> */}
+
       <div className="container">
         <div className="row">
-          <div id="tab1" className="tabs_item">
-            <div className="row justify-content-center">
-              <div className="col-lg-4 col-md-6 col-sm-6">
-                <ul className="tabs">
-                  <li
-                    className="current"
-                    onClick={(e) => openTabSection(e, 'tab1')}
-                  >
-                    Monthly Plan
-                  </li>
-
-                  <li onClick={(e) => openTabSection(e, 'tab2')}>
-                    Yearly Plan
-                  </li>
-                </ul>
-                <div className="pricing-box">
-                  <div className="pricing-header">
-                    <h3>FREE</h3>
-                    <p>Get your business up <br /> and running</p>
-                  </div>
-
-                  <div className="price">
-                    &#8377;0 <span>/m</span>
-                  </div>
-
-                  <Link to="#" className="btn btn-primary rounded-pill mb-4">
-                    Get Started 30 days (1 user)
-                  </Link>
-
-                  <ul className="pricing-features">
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Email Support Only</li>
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Web & Mobile CRM</li>
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Auto Lead Distribution</li>
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Tasks & Reminders</li>
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Customized Calling Structure</li>
-                    <li><i className="fa fa-check" style={{ color: "#275DE1" }}></i>&nbsp;Real-time Reports</li>
-                  </ul>
+          {/* Level 1 */}
+          <div className="col-md-4">
+            <div className="level_heading_card">
+              <p className='level_heading'>Level 1 - Beginner</p>
+            </div>
+            <div className="row mb-3">
+              {["1 week", "10 Days", "2 Weeks"].map((option) => (
+                <div className="col-4" key={option}>
+                  <button className='week_btn' onClick={() => handleButtonClick('level1', option)}>
+                    {option}
+                  </button>
                 </div>
+              ))}
+              <div className="card px-3 h-100 mb-4 bg-transparent text-center">
+                <p className='text-center'>{levelData.level1[selectedOption.level1].price}</p>
+                {levelData.level1[selectedOption.level1].para.map((item, index) => (
+                  <p key={index} className='text-center'>
+                    <span>
+                      <img className='me-3' src={item.image} alt="icon" style={{ maxWidth: '30px' }} />
+                    </span>
+                    {item.text}
+                  </p>
+                ))}
               </div>
+            </div>
+          </div>
 
-              {/* Other plans omitted for brevity */}
+          {/* Level 2 */}
+          <div className="col-md-4">
+            <div className="level_heading_card">
+              <p className='level_heading'>Level 2 - Intermediate</p>
+            </div>
+            <div className="row mb-3">
+              {["1 week", "10 Days", "2 Weeks"].map((option) => (
+                <div className="col-4" key={option}>
+                  <button className='week_btn' onClick={() => handleButtonClick('level2', option)}>
+                    {option}
+                  </button>
+                </div>
+              ))}
+              <div className="card px-3 h-100 mb-4 bg-transparent text-center">
+                <p className='text-center'>{levelData.level2[selectedOption.level2].price}</p>
+                {levelData.level2[selectedOption.level2].para.map((item, index) => (
+                  <p key={index} className='text-center'>
+                    <span>
+                      <img className='me-3' src={item.image} alt="icon" style={{ maxWidth: '30px' }} />
+                    </span>
+                    {item.text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
 
+          {/* Level 3 */}
+          <div className="col-md-4">
+            <div className="level_heading_card">
+              <p className='level_heading'>Level 3 - Advanced</p>
+            </div>
+            <div className="row mb-3">
+              {["1 week", "10 Days", "2 Weeks"].map((option) => (
+                <div className="col-4" key={option}>
+                  <button className='week_btn' onClick={() => handleButtonClick('level3', option)}>
+                    {option}
+                  </button>
+                </div>
+              ))}
+              <div className="card px-3 h-100 mb-4 bg-transparent text-center">
+                <p className='text-center'>{levelData.level3[selectedOption.level3].price}</p>
+                {levelData.level3[selectedOption.level3].para.map((item, index) => (
+                  <p key={index} className='text-center'>
+                    <span>
+                      <img className='me-3' src={item.image} alt="icon" style={{ maxWidth: '30px' }} />
+                    </span>
+                    {item.text}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Included;
