4ae10f61439118e740440d223a1b1b9fe2230f76
Rishabh Chaurasia <rishabhchaurasia456@gmail.com>
2024-11-21 16:26:53 +0530

    file add

---
+++ src/Components/PageTracker.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const PageTracker = ({ children }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Get the stored userId from localStorage
+        const userId = localStorage.getItem('userId');
+
+        if (userId) {
+            // Track the page visit
+            trackVisit(userId, location.pathname);
+        }
+    }, [location]);
+
+    const trackVisit = (userId, page) => {
+        fetch('http://localhost:5000/track', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id: userId, page: page })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Visit tracked:", data);
+        })
+        .catch(error => {
+            console.error("Error tracking visit:", error);
+        });
+    };
+
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default PageTracker;
