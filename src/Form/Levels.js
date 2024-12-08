1114028141ce2c7cd9c88c020a463fb69943412e
Rishabh Chaurasia <rishabhchaurasia456@gmail.com>
2024-12-08 11:26:12 +0530

    text change

--- src/Form/Levels.js
+++ src/Form/Levels.js
@@ -198,7 +198,7 @@ const Levels = () => {
                           >
                             {index === 0
                               ? 'Shared Kite Rental – Always Included (€60 x person x days)'
-                              : `Rental Of Complete kiteset for this level (€60 x  person x days)`}
+                              : `Rental Of Complete kiteset (€60 x  person x days)`}
                           </label>
                         </div>
                       </div>
